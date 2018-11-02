c9c79dabbf765f83aa74281839d8e3485bb1409c
mewlist <mewlist@mewlist.com>
2018-11-02 12:54:03 +0900

    Initial Commit

---
+++ Assets/VolumeTextureViewer/Script/Rotater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rotater : MonoBehaviour
+{
+
+	public float speed = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, speed * Time.deltaTime, 0);
+	}
+}
